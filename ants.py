440e9c480e5a8e0ee71f4cea79800a98904a5074
Marco Secci <marcosecci2002@gmail.com>
2023-10-19 11:27:25 +0200

    added documentation

--- ants.py
+++ ants.py
@@ -11,17 +11,46 @@ import random
 # =============== #
 class AntAgent:
     """
-    ## AntAgent class
+    ## `AntAgent` class
 
     ====================================
 
-    #### Description:
+    #### Description
 
     Creates the various ant agents that exist in an anthouse, like the Queen, workers, larvae, eggs, etc
     and give them attributes like their lifespan, their hatching time, their task, if they're holding something, etc.
     """
 
-    def __init__(self, x, y, agent_type):
+    # =========== #
+    # INIT METHOD #
+    # =========== #
+    def __init__(self, x: float, y: float, agent_type: str):
+        """
+        ## `__init__` method
+
+        ====================================
+
+        #### Description
+
+        Creates an ant. It gives it a position, a role, and check if it has laid egg (only queens can lay eggs).
+
+        ====================================
+
+        #### Parameters
+
+        #### - `x`: `float`
+            position of the ant on the `x` axis;
+
+        #### - `y`: `float`
+            position of the ant on the `y` axis;
+
+        #### - `agent_type`: `str`
+            title or role of the ant; choose from list:
+            - Queen
+            - Egg
+            - Larvae
+            - Worker
+        """
         self.x = x
         self.y = y
         self.agent_type = agent_type
@@ -33,11 +62,11 @@ class AntAgent:
 # ===================== #
 class AntEnvironment:
     """
-    ## AntEnvironment class
+    ## `AntEnvironment` class
 
     ====================================
 
-    #### Description:
+    #### Description
 
     Creates the starting environment in which the ants will operate and create their settlement.
     It's in 2D for now but will soon become 3D as an anthouse has multiple layers.
@@ -52,14 +81,14 @@ class AntEnvironment:
 
         ====================================
 
-        #### Description:
+        #### Description
 
         Creates the starting environment in which the ants will operate and create their settlement.
         It's in 2D for now but will soon become 3D as an anthouse has multiple layers.
 
         ====================================
 
-        #### Parameters:
+        #### Parameters
 
         #### - `width`: `int`
             width of the environment;
@@ -70,4 +99,4 @@ class AntEnvironment:
         self.width = width
         self.height = height
         # Creating an empty environment - 0 = empty
-        self.grid = np.zeros((width, height))
+        self.grid = np.zeros((width, height), dtype=object)
