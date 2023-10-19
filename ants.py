6b87d50aad61beed7e141998641c04a50050907f
Marco Secci <marcosecci2002@gmail.com>
2023-10-19 12:06:02 +0200

    created the first working version of the environment with Queen and Eggs. Added main.py

--- ants.py
+++ ants.py
@@ -100,3 +100,76 @@ class AntEnvironment:
         self.height = height
         # Creating an empty environment - 0 = empty
         self.grid = np.zeros((width, height), dtype=object)
+
+    # ================ #
+    # ADD AGENT METHOD #
+    # ================ #
+    def add_agent(self, x: float, y: float, agent_type: str):
+        """
+        ## `add_agent` method
+
+        ====================================
+
+        #### Description
+
+        Collocates an `Agent` of the specified type in the environment,
+        at the chosen coordinates.
+
+        ====================================
+
+        #### Parameters
+
+        #### - `x`: `float`
+            `x` coordinate of the first location of the `Agent`
+
+        #### - `y`: `float`
+            `y` coordinate of the first location of the `Agent`
+        """
+        agent = AntAgent(x, y, agent_type)
+        self.grid[x, y] = agent
+
+    # ======================= #
+    # EXPAND TERRITORY METHOD #
+    # ======================= #
+    def expand_territory(self, x: float, y: float):
+        """
+        ## `expand_territory` method
+
+        ====================================
+
+        #### Description
+
+        Expands the territory controlled by the ant colony. In this early stage, it's made by only laying eggs.
+        TODO: every square of the grid will have an ID that indicates its state: if it's free, if there's an obstacle,
+        food, or if it has been colonized by the ants.
+
+        ====================================
+
+        #### Parameters
+
+        #### - `x`: `float`
+            `x` coordinate of the territory that will be checked for expansion
+
+        #### - `y`: `float`
+            `y` coordinate of the territory that will be checked for expansion
+        """
+        # Simulate the queen expanding her territory
+        if self.grid[x, y] is None:
+            self.add_agent(x, y, "Queen")
+        elif self.grid[x, y].agent_type == "Egg":
+            self.grid[x, y].has_laid_egg = True
+
+    # ============== #
+    # DISPLAY METHOD #
+    # ============== #
+    def display(self):
+        for row in self.grid:
+            row_str = ""
+            for cell in row:
+                if cell is None:
+                    row_str += " "
+                elif isinstance(cell, AntAgent):
+                    row_str += cell.agent_type[0]
+                else:
+                    row_str += str(cell)
+            print(row_str)
---
+++ main.py
@@ -0,0 +1,22 @@
+import ants
+
+environment = ants.AntEnvironment(width=10, height=10)
+
+queen_x = 1
+queen_y = 1
+
+environment.add_agent(queen_x, queen_y, "Queen")
+
+# Laying eggs
+egg_positions = [
+    (queen_x + 1, queen_y),
+    (queen_x - 1, queen_y),
+    (queen_x + 2, queen_y + 3),
+]
+
+for egg_x, egg_y in egg_positions:
+    environment.add_agent(egg_x, egg_y, "Egg")
+
+# Display environment
+
+environment.display()
