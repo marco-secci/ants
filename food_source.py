4e820be71eba8eec521a38d87e8d9d0de83b9023
Marco Secci <marcosecci2002@gmail.com>
2023-10-25 21:57:20 +0200

    added food source class

--- food_source.py
+++ food_source.py
@@ -1,9 +1,18 @@
+# ================= #
+# FOOD SOURCE CLASS #
+# ================= #
 class FoodSource:
+    # =========== #
+    # INIT METHOD #
+    # =========== #
     def __init__(self, x, y, quantity):
         self.x = x
         self.y = y
         self.quantity = quantity
 
+    # ============== #
+    # DEPLETE METHOD #
+    # ============== #
     def deplete(self, amount):
         """
         Removing an amount of food from the food source because of various causes, such as ants
